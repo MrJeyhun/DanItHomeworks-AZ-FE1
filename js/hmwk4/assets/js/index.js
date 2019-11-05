97cc030b328af8ab614efe07f392bb85fa3daf2e
MrJeyhun <46054708+MrJeyhun@users.noreply.github.com>
2019-11-05 11:34:33 +0400

    "Homework4(js) was added"

---
+++ js/hmwk4/assets/js/index.js
@@ -0,0 +1,32 @@
+//create new user
+function createNewUser(newUser) {
+  //ask for name and surname
+  let name = prompt("Give me your name dude! ");
+  let surname = prompt("and your surname");
+  //return object
+  return {
+    firstname: name,
+    lastname: surname,
+    getLogin: function() {
+      //return as abb
+      return (
+        this.firstname.charAt(0).toLowerCase() + this.lastname.toLowerCase()
+      );
+    },
+    setFirstName: function() {
+      let newName = prompt("You are editing your name, don't be shy");
+      this.firstname = newName;
+      return this.firstname;
+    },
+    setLastName: function() {
+      let newLastName = prompt(
+        "You are editing your last name, did you married?) "
+      );
+      this.lastname = newLastName;
+      return this.surname;
+    }
+  };
+}
+
+let newUser = new createNewUser();
+console.log(newUser.getLogin());
